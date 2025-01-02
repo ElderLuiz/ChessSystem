07ce8444cf66c679988ec25b15ba2231007bbf76
Elder Luiz <elderluizmoreira1@gmail.com>
2025-01-02 17:27:47 -0300

    Concertando o erro do nélio

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,14 +1,9 @@
 package chess;
 
-<<<<<<< HEAD
 import boardgame.BoardException;
 
 public class ChessException extends BoardException {
     public ChessException(String msg) {
-=======
-public class ChessException extends RuntimeException{
-    public ChessException(String msg){
->>>>>>> origin/main
         super(msg);
     }
 }
